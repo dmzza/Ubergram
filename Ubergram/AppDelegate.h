67f7ccaa501c4c78c769d76b68bf7682fde6bdd7
David Mazza <david@fleksy.com>
2015-02-06 10:49:11 -0800

    Initial Commit

---
+++ Ubergram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ubergram
+//
+//  Created by David Mazza on 2/6/15.
+//  Copyright (c) 2015 David Mazza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ubergram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ubergram
+//
+//  Created by David Mazza on 2/6/15.
+//  Copyright (c) 2015 David Mazza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
